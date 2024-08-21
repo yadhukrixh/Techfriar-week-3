31dbb6e7a33d65cfa4c6b7b20b5e826c90635452
Yadhukrishnan <yadhukrishnan2030@gmail.com>
2024-08-21 17:46:15 +0530

    deployment test-2

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -72,10 +72,7 @@ const Footer: React.FC = () => {
                             <img src="Photos/mobile-footer-logo.svg" alt="Al Habtoor Group Logo" className={styles.mobileLogo} />
                         </div>
                         <div className={styles.mobileFooterIcons}>
-                            <a href="#"><img src="Photos/icons/facebook-inverted-color.svg" alt="Facebook" className={styles.mobileFooterIcon} /></a>
-                            <a href="#"><img src="Photos/icons/x-inverted-color.svg" alt="Twitter" className={styles.mobileFooterIcon} /></a>
-                            <a href="#"><img src="Photos/icons/instagram-inverted-color.svg" alt="Instagram" className={styles.mobileFooterIcon} /></a>
-                            <a href="#"><img src="Photos/icons/linkdin-inverted-color.svg" alt="LinkedIn" className={styles.mobileFooterIcon} /></a>
+                            
 
                         </div>
                     </div>
